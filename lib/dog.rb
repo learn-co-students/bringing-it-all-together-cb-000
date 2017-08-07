9b9f9eaae8e6da82ef0743c532c4ade3005468c8
cptbrkfst <dyl@resonategroup.ca>
2017-08-07 15:55:27 -0400

    added methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,12 +27,16 @@ class Dog
   end
 
   def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed) VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
-    self
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
+      self
+    end
   end
 
   def self.create(name:, breed:)
@@ -42,16 +46,29 @@ class Dog
   end
 
   def self.find_by_id(dog_id)
-    row = DB[:conn].execute('SELECT * FROM dogs WHERE id = ?', dog_id)
+    row = DB[:conn].execute('SELECT * FROM dogs WHERE id = ? LIMIT 1', dog_id)
     dog = new_from_db(row)
     dog
   end
 
   def self.new_from_db(row)
-    row.flatten!
+    row = row.flatten
     id, name, breed = row[0], row[1], row[2]
     dog = self.new(name: name, breed: breed)
     dog.id = id
     dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name LIKE ?
+    SQL
+    row = DB[:conn].execute(sql, name).flatten
+    self.new_from_db(row)
+  end
+
+  def update
+    sql = 'UPDATE dogs SET name = ?, breed = ? WHERE id = ?'
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 end
