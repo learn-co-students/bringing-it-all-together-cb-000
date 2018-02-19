059540deef8a5d43a9228a192c728cd4df177aec
Jen Poole <contact.vandepool@gmail.com>
2018-02-19 20:49:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,4 +87,19 @@ class Dog
     end.first
   end
 
+
+  # creates an instance of a dog if it does not already exist
+  # when two dogs have the same name and different breed, it returns the correct dog
+  # when creating a new dog with the same name as persisted dogs, it returns the correct dog
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}'")
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end
+
 end
