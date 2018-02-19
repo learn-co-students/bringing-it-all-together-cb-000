7d5988d11195d4e9b408eaaf00370c4ffc1a7228
Jen Poole <contact.vandepool@gmail.com>
2018-02-19 20:50:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,6 @@ class Dog
     end.first
   end
 
-
   # creates an instance of a dog if it does not already exist
   # when two dogs have the same name and different breed, it returns the correct dog
   # when creating a new dog with the same name as persisted dogs, it returns the correct dog
@@ -100,6 +99,19 @@ class Dog
       dog = self.create(name: name, breed: breed)
     end
     dog
+  end
+
+  # returns an instance of dog that matches the name from the DB
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
