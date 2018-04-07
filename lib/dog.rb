cd9c3786326436e1f6fc011ec742c194007995a7
Liz Jenerson <liz.jenerson@gmail.com>
2018-04-07 19:51:22 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,18 +15,36 @@ class Dog
   end
 
   def save
-    if self.id
-      self.update
-    else
-      sql = <<-SQL
-        INSERT INTO dogs (name, breed)
-        VALUES (?, ?)
-        SQL
+
+=begin
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+
     DB[:conn].execute(sql, self.name, self.breed)
+=end
+  end
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  def self.new_from_db(row)
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+    new_dog
+  end
 
-    end
+  def self.find_by_name(name)
+
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+      LIMIT 1
+      SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+
   end
 
 end
