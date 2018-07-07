c33f1c291c1aa82c74dc74f079939cf57d658f90
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-07 01:20:03 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,80 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+
+  def initialize(name:, breed:, id: nil)
+    @name, @breed, @id = name, breed, id
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT)
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(name:, breed:)
+    dog = self.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+      SQL
+    dog = DB[:conn].execute(sql, id)[0]
+    self.new(id: dog[0], name: dog[1], breed: dog[2])
+  end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", [name, breed])
+    if !sql.empty?
+      dog = sql.flatten
+      new_dog = self.new(id: dog[0], name: dog[1], breed:dog[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+  end
+
+  def self.new_from_db(row)
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
+    new_dog
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      SQL
+    dog = DB[:conn].execute(sql, name)[0]
+    self.new(id: dog[0], name: dog[1], breed: dog[2])
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
+end
